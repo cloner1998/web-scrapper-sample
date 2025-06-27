64a5faee7df84ab191b323bf9b0cbfee46991212
cloner1998 <sajjadyami77@gmail.com>
2025-06-27 22:13:10 +0330

    better example for getting image from a sit

--- web_scapp.py
+++ web_scapp.py
@@ -1,22 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
-import cssutils
 
-r = requests.get("https://jadi.ir/")
-print("scrapping 'https://jadi.ir/' : ")
+r = requests.get("https://en.wikipedia.org/wiki/Python_(programming_language)")
+print("scrapping 'https://en.wikipedia.org/wiki/Python_(programming_language)' : ")
 print(f" status code : {r.status_code}")
 print("_________")
 
-jadi_soup = BeautifulSoup(r.content, 'html.parser')
-web_title = jadi_soup.find('title')
+soup = BeautifulSoup(r.content, 'html.parser')
+web_title = soup.find('title')
 print(f"title of this web page is : {web_title.text}")
 print("_________")
 
-web_class = jadi_soup.find(class_='files')
-print(f"one class of this web page is : {web_class.text}")
+web_classes = soup.find_all(class_="vector-toc-text")
+# print(web_class)
+for web_class in web_classes:
+    print(f" class : {web_class.text}")
 print("_________")
 
-body_style = jadi_soup.find('body')['style']
-style = cssutils.parseStyle(body_style)
-print(style['background-image'])
+img_python = soup.find_all("img", class_="mw-file-element")
+for img in img_python:
+    print(f" img : {img['src']}")
+
 print("_________")
